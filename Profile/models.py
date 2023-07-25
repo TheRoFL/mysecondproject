742cd9d35a891b712c495dce4447c1653265abac
TheRoFL <gabellaoff@yandex.ru>
2023-07-26 01:04:56 +0500

    добавил фон для всех хтмл

--- Profile/models.py
+++ Profile/models.py
@@ -8,13 +8,15 @@ class ProfileData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=20,blank=False, null=False)
     surname = models.CharField(max_length=20, blank=False, null=False)
-    birthdate = models.DateField(blank=True, null=True)
 
     SEX_CHOICES = (('m', 'Male'),('f', 'Female'))
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=False, null=False)
 
-
+    birthdate = models.DateField(blank=True, null=True)
+    city = models.CharField(max_length=50,blank=True, null=True)
+    address = models.CharField(max_length=50,blank=True, null=True)
     
+    bonuses = models.IntegerField(default=0, blank=True, null=True)
     age = models.IntegerField(default=18, blank=False, null=False)
     def __str__(self):
         return self.user.username
