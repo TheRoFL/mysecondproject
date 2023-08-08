9014afbe54f43cfa8db6aaacbffd3100fc1548d7
Ilyuska <132598878+Ilyuska@users.noreply.github.com>
2023-08-08 23:28:13 +0500

    You're gey

---
+++ static/js/test.js
@@ -0,0 +1,33 @@
+function wind() {
+  const div = `<div class = "overflow hidden"></div>
+  <div class="modWind hidden">
+    <div class="name">Борщ</div>
+    <div class="grams">200гр</div>
+    <p class="kbju">Белки 10г Жиры 10г Угл 10г Ккал 100</p>
+    <img src="" alt="" class="food" />
+    <p class="sostav">АА,bb,cc</p>
+</div>
+`;
+  document.querySelector(".dishes").insertAdjacentHTML("beforebegin", div);
+}
+
+const ws = document.querySelectorAll(".dishes");
+for (let i = 0; i < ws.length; i++) {
+  wind();
+  ws[i].addEventListener("click", () => {
+    x.classList.remove("hidden");
+    y.classList.remove("hidden");
+  });
+}
+
+const x = document.querySelector(".modWind");
+const y = document.querySelector(".overflow");
+
+const button = document.querySelector(".dishes");
+
+document.addEventListener("keydown", (e) => {
+  if (e.code == "Escape") {
+    x.classList.add("hidden");
+    y.classList.add("hidden");
+  }
+});
