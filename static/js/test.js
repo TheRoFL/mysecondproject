de634458953ab68390ea8fe1af85663b04dcadff
Ilyuska <132598878+Ilyuska@users.noreply.github.com>
2023-08-09 22:45:51 +0500

    s

--- static/js/test.js
+++ static/js/test.js
@@ -1,25 +1,26 @@
-function wind() {
-  const div = `<div class = "overflow hidden"></div>
-  <div class="modWind hidden">
-    <div class="name">Борщ</div>
-    <div class="grams">200гр</div>
-    <p class="kbju">Белки 10г Жиры 10г Угл 10г Ккал 100</p>
-    <img src="" alt="" class="food" />
-    <p class="sostav">АА,bb,cc</p>
-</div>
-`;
-  document.querySelector(".dishes").insertAdjacentHTML("beforebegin", div);
-}
-
 const ws = document.querySelectorAll(".dishes");
-for (let i = 0; i < ws.length; i++) {
-  wind();
+console.log(ws.length);
+for (let i = 1; i < ws.length; i++) {
+  const div = `<div class = "overflow hidden"></div>
+    <div class="modWind hidden">
+    <div ><img style = "width: 200px"
+    src="http://localhost:8000/media/menu_images/${ws[i].getAttribute(
+      "data-type"
+    )}/${ws[i].getAttribute("data-name")}.png"
+    /> </div>
+      <div class="name">${ws[i].getAttribute("data-name")}</div>
+      <div class="grams">${ws[i].getAttribute("data-weight")} гр</div>
+      <div class="price">${ws[i].getAttribute("data-price")} руб</div>
+      <div class="sostav">${ws[i].getAttribute("data-sostav")}</div>
+  </div>
+  `;
+  document.querySelector(".dishes").insertAdjacentHTML("beforebegin", div);
+
   ws[i].addEventListener("click", () => {
     x.classList.remove("hidden");
     y.classList.remove("hidden");
   });
 }
-
 const x = document.querySelector(".modWind");
 const y = document.querySelector(".overflow");
 
